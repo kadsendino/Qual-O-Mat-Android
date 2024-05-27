bd78e8425bb31bb4b26fc804a33ebb9bc8d4b9ae
kadsendino <maxii.prietzel@gmail.com>
2024-05-27 22:42:44 +0200

    try and error stuff

---
+++ app/src/main/java/com/example/qual_o_mat/ui/about/AboutFragment.java
@@ -0,0 +1,37 @@
+package com.example.qual_o_mat.ui.about;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.qual_o_mat.databinding.FragmentHomeBinding;
+
+public class AboutFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        AboutViewModel homeViewModel =
+                new ViewModelProvider(this).get(AboutViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
